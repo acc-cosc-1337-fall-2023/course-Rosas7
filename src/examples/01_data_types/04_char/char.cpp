5fe83e6a09f18bbb646493ef14de9b024b903b86
Rosas7 <143145458+Rosas7@users.noreply.github.com>
2023-09-06 00:55:41 +0000

    intro boolean expressions

--- src/examples/01_data_types/04_char/char.cpp
+++ src/examples/01_data_types/04_char/char.cpp
@@ -1,5 +1,5 @@
 //cpp
-
+#include "char.h"
 
 /*
 Function get_char_ascii_value with a char parameter
@@ -7,3 +7,44 @@ Function get_char_ascii_value with a char parameter
 @param char-a character
 @return the ASCII value of the character
 */
+int get_char_ascii_value(char ch)
+{
+    return ch;
+}
+
+bool get_and_result(int num1, int num2)
+{
+    bool result = num1 ==3 && num2 == 5;
+
+    return result;
+}
+
+bool get_or_result(bool bool1, bool bool2)
+{
+    bool result = bool1 || bool2;
+    return result;
+}
+
+bool get_bool_negation(bool bool1)
+{
+    return !bool1;
+}
+
+bool get_order_of_precedence_result(bool bool1, bool bool2, bool bool3)
+{
+    bool result = !bool1 && bool2 || bool3;
+    return result;
+}
+
+bool is_letter_a_consonant(char letter)
+{
+    bool result = letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u';
+    return !result;
+}
+
+bool is_number_not_in_range(int num1, int low_range, int max_range)
+{
+    bool result = num1 < low_range || num1 > max_range;
+
+    return result;
+}
--- test/examples/01_data_types/01_data_types_tests.cpp
+++ test/examples/01_data_types/01_data_types_tests.cpp
@@ -1,5 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "char.h"
+#include "char.cpp"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -16,6 +18,51 @@ TEST_CASE("Verify Test Configuration", "verification") {
 /*test case add_to_double_2 with 1 as parameter*/
 
 /*test case char test ASCII values */
+TEST_CASE("Test the get_char ascii value function", "char to int conversion ")
+{
+	REQUIRE(65 == get_char_ascii_value('A'));
+	REQUIRE(97 == get_char_ascii_value('a'));
+	REQUIRE(98 == get_char_ascii_value('b'));
+
+}
+
 
 /*test case string test ASCII values Joe characters are equal to 74, 111, and 101*/
 
+/**Test boolean operators*/
+TEST_CASE("Test the and operator truth table", "only T T returns T")
+{
+	REQUIRE(get_and_result(3, 5) == true);
+	REQUIRE(get_and_result(5, 3) == false);
+	REQUIRE(get_and_result(3, 1) == false);
+	REQUIRE(get_and_result(1, 5) == false);
+}
+
+TEST_CASE("Test the or operator truth table", "only F F returns F")
+{
+	REQUIRE(get_or_result(true, true) == true);
+	REQUIRE(get_or_result(true, false) == true);
+	REQUIRE(get_or_result(false, true) == true);
+	REQUIRE(get_or_result(false, false) == false);
+}
+
+TEST_CASE("Test the negation operator truth table", "negate values")
+{
+	REQUIRE(get_bool_negation(true) == false);
+	REQUIRE(get_bool_negation(false) == true);
+}
+
+TEST_CASE("Test the boolean operators order of precedence", "order of operations")
+{
+	REQUIRE(get_order_of_precedence_result(true, true, true) == true);
+	REQUIRE(get_order_of_precedence_result(true, true, false) == false);
+
+}
+
+TEST_CASE("Test a use case for boolean operators", "negate a result")
+{
+	REQUIRE(is_letter_a_consonant('a') == false);
+	REQUIRE(is_letter_a_consonant('z') == true);
+}
+
+TEST_CASE("Test ")
