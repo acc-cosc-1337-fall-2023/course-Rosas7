dbdbd8ee5f5f121b84f8f15d30388c1a9ec1a26a
Rosas7 <143145458+Rosas7@users.noreply.github.com>
2023-09-24 21:24:30 +0000

    homework#4

--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,8 +1,57 @@
 //write include statements
+#include "repetition.h"
 
 //write using statements
 
+using std::cout; using std::cin;
+
 int main() 
 {
+	int option;
+
+	do
+	{
+		cout<<"Menu\n";
+		cout<<"1-Factorial\n";
+		cout<<"2-Greatest Common Divisor\n";
+		cout<<"3-Exit\n";
+		auto option = 0;
+		cout<<"Enter a menu option\n";
+		cin>>option;
+
+		switch(option)
+		{
+		case 1:
+			int num;
+			cout<<"Enter a number: ";
+			cin>>num;
+			cout<<"The Factorial of "<<num<<" is "<<factorial(num)<<"\n";
+			break;
+		case 2:
+			int num1, num2;
+			cout<<"Enter two numbers to find their gcd: \n";
+			cout<<"Number 1: ";
+			cin>>num1;
+			cout<<"Number 2: ";
+			cin>>num2;
+			cout<<"The gcd of "<<num1<< " and "<<num2<<" is "<<gcd(num1, num2)<<"\n";
+			break;
+		case 3:
+			char answer;
+			cout<<"Are you sure you want to exit? (y/n): ";
+			cin>>answer;
+			if(answer == 'y' || answer == 'Y')
+			{
+				cout<<"Goodbye....";
+				return 0;
+			}
+			break;
+		default:
+			cout<<"Invalid Option";
+			break;
+		}
+	} 
+	while (option != 3);
+	
 	return 0;
 }
--- src/homework/04_repetition/repetition.cpp
+++ src/homework/04_repetition/repetition.cpp
@@ -1,3 +1,37 @@
 //add include statements
+#include "recursion.h"
 
 //add function(s) code here
+int factorial(int num)
+{
+    auto result = 1;
+
+    while(num > 0)
+    {
+        result *= num;
+        num --; 
+    }
+
+    return result;
+
+}
+
+int gcd(int num1, int num2)
+{
+    while(num1 != num2)
+    {
+        if(num1 < num2)
+        {
+            int tempVal = num1;
+            num1 = num2;
+            num2 = tempVal;
+        }
+
+        else
+        {
+            num1 -= num2;
+        }
+
+    }
+    return num1;
+}
