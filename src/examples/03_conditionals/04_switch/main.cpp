f919e23baaecba5d9850ad6c811a2eb1f5bf138d
Rosas7 <143145458+Rosas7@users.noreply.github.com>
2023-09-24 17:01:12 +0000

    menu example

--- src/examples/03_conditionals/04_switch/main.cpp
+++ src/examples/03_conditionals/04_switch/main.cpp
@@ -1,17 +1,9 @@
-#include<iostream>
 #include "switch.h"
 
-using std::cout; using std::cin; using std::string;
 
 int main() 
 {
-	auto option = 0;
-	cout<<"Enter a numer: ";
-	cin>>option;
-
-	string result = menu(option);
-	cout<<result<<"\n";
-	
+	menu();
 
 	return 0;
 }
