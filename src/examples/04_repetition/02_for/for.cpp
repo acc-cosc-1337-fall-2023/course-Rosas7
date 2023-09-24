d9b9d42698d9bf8430257ff8db745f24f43d3e7a
Rosas7 <143145458+Rosas7@users.noreply.github.com>
2023-09-24 17:30:18 +0000

    for loop

--- src/examples/04_repetition/02_for/for.cpp
+++ src/examples/04_repetition/02_for/for.cpp
@@ -1,6 +1,9 @@
 #include "for.h"
 #include<iostream>
 
+using std::cout;
+
+
 //Write code for function display_numbers that accepts a number and 
 //displays every number that precedes(starting from 1) it in a line
 //Example: display_numbers(5)
@@ -14,4 +17,23 @@ RESULT:
 5
 
 */
+void display_numbers(int num)
+{
+    for(auto i=0; i < num; i++)
+    {
+        cout<<i+1<<"\n";
+    }
+}
+
+int sum_of_squares_for(int num)
+{
+    auto sum = 0;
+
+    for(auto i = 0; i < num; i++)
+    {
+        sum += (i+1) * (i+1);
+    }
+    
+    return sum;
+}
 
--- src/examples/04_repetition/02_for/main.cpp
+++ src/examples/04_repetition/02_for/main.cpp
@@ -1,8 +1,11 @@
 #include"for.h"
+using std::cout;
 
 int main() 
 {
-	
+	display_numbers(5);
+	auto result = sum_of_squares_for(5);
+	cout<<"Result is: "<<result<<"\n";
 
 	return 0;
 }
