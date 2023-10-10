050feae9ecdb8318bad61b8f7c7dc627852daf99
Rosas7 <143145458+Rosas7@users.noreply.github.com>
2023-10-10 02:50:43 +0000

    homework #5

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,55 @@
 //add include statements
+#include "func.h"
 
 //add function code here
+double get_gc_content(const string& dna)
+{
+    int gc_content = 0;
+    int dna_length = dna.size();
+
+    for(char i : dna)
+    {
+        if( i == 'G' || i == 'C' )
+        {
+            gc_content ++;
+        }
+    }
+
+    return float(gc_content) / dna_length; 
+}
+
+string get_reverse_string(string dna)
+{
+    string reverse;
+    for(char i= dna.size() - 1; i>=0; i--)
+    {
+        reverse += dna[i];
+    }
+
+    return reverse;
+}
+
+string get_dna_complement(string dna)
+{
+    string reverse = get_reverse_string(dna);
+    for(char i = 0; i< reverse.size(); i++)
+    {
+        if(reverse[i] == 'A')
+        {
+            reverse[i] = 'T';
+        }
+        else if(reverse[i] == 'T')
+        {
+            reverse[i] = 'A';
+        }
+        else if(reverse[i] == 'C')
+        {
+            reverse[i]= 'G';
+        }
+        else if(reverse[i] == 'G')
+        {
+            reverse[i] = 'C';
+        }
+    }
+    return reverse;
+}
--- src/homework/05_functions/main.cpp
+++ src/homework/05_functions/main.cpp
@@ -1,6 +1,47 @@
+#include<iostream>
+#include "func.h"
+
+using std::cout;
+using std::cin;
 
 int main() 
 {
+	auto option = 0;
+
+	do
+	{
+		cout<<"Menu\n";
+		cout<<"1-Get GC Content\n";
+		cout<<"2-Get DNA Complement\n";
+		cout<<"3-Exit\n\n";
+		cout<<"Enter a menu option: ";
+		cin>>option;
+
+		if (option == 1)
+		{
+			string content;
+			cout<<"Enter a DNA string: ";
+			cin>>content;
+			cout<<"The GC Content of "<<content<<" is "<<get_gc_content(content)<<"\n\n";
+		}
+		else if (option == 2)
+		{
+			string complement;
+			cout<<"Enter a DNA string: ";
+			cin>>complement;
+			cout<<"The DNA Complement of "<<complement<<" is "<<get_dna_complement(complement)<<"\n\n";
+		}
+		else if(option ==3)
+		{
+			cout<<"Exiting program....\n";
+		}
+		else
+		{
+			cout<<"Invalid option\n\n";
+		} 
+	}
+	while (option != 3);
 
 	return 0;
+
 }
