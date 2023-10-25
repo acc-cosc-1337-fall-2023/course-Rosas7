a5e824a772477eb2a1a98bf4dce70b62273780c8
Rosas7 <143145458+Rosas7@users.noreply.github.com>
2023-10-25 00:30:26 +0000

    pointers

--- src/examples/08_classes/01_bank/main.cpp
+++ src/examples/08_classes/01_bank/main.cpp
@@ -1,6 +1,43 @@
+#include "bank_account.h"
+#include<time.h>//helps provide a true random number with rand() function
+#include "atm.h"
+#include "savings_account.h"
+#include "checking_account.h"
+
+using std::cout;
 
 int main()
 {
+	srand(time(NULL));
+	BankAccount* account;//create a pointers; points to nothing NULL
 
+	CheckingAccount checking_account(500);
+	account = &checking_account;
+	cout<<account->get_balance()<<"\n";	
+	
+	SavingsAccount savings_account(500);
+	account = &savings_account;
+	cout<<account->get_balance()<<"\n";
+
+	//account.deposit(250);
+
+	/*ATM atm(account);
+
+	atm.display_balance();
+	atm.make_deposit();
+	atm.display_balance();
+
+	cout<<"Balance: "<<account.get_balance()<<"\n";
+
+	atm.make_withdrawl();
+
+	atm.display_balance();
+	cout<<"Balance: "<<account.get_balance()<<"\n";
+
+	BankAccount account2;//customer 2
+	cout<<"Balance: "<<account2.get_balance()<<"\n";
+
+	show_balance(account);
+	*/
 	return 0;
 }
--- src/examples/08_classes/01_bank/savings_account.h
+++ src/examples/08_classes/01_bank/savings_account.h
@@ -1,1 +1,15 @@
 //savings_account.h
+#include "bank_account.h"
+
+#ifndef SAVINGS_ACCOUNT_H
+#define SAVINGS_ACCOUNT_H
+
+class SavingsAccount: public BankAccount //inherit from BankAccount
+{
+public:
+    SavingsAccount(){/**EMPTY CODE BLOCK*/}
+    SavingsAccount(int b) : BankAccount(b) {/*EMPTY CODE BLOCK*/}
+    int get_balance() const {return BankAccount::get_balance() + 5;}
+};
+
+#endif
